9ed088a882c0a11377d578890ee8d556859ad7b4
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-06-20 21:57:50 +0200

    check functionality of doppler

--- src/env.mjs
+++ src/env.mjs
@@ -8,6 +8,7 @@ export const env = createEnv({
    */
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
+    DOPPLER_WORKS: z.coerce.boolean(),
   },
 
   /**
@@ -25,6 +26,7 @@ export const env = createEnv({
    */
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
+    DOPPLER_WORKS: process.env.DOPPLER_WORKS,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
